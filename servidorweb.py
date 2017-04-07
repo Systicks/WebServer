eba2dd0684577a851e77c253ef25c60a39d8efbf
Systick <carlos.perezrivas@edu.uah.es>
2017-04-07 11:14:19 +0200

    Update

--- servidorweb.py
+++ servidorweb.py
@@ -2,6 +2,7 @@ import socket, select, string, sys, json
 
 cabecera = 'HTTP/1.0 200 OK\r\nDate: Friday, 6-May-11 15:40:00 GMT\r\nServer: Apache/1.1.1\r\nContent-type: text/html\r\nContent-length: 450\r\nConnection: close\r\n'
 
+
 if __name__ == "__main__":
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind(("", 8000))
