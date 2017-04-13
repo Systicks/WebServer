56d95d0450c0397cd114b0286b0adb9ea6797b1d
Systicks <carlos_uceda7@hotmail.com>
2017-04-13 11:38:05 +0200

    Mp3 format working properly

--- servidorweb.py
+++ servidorweb.py
@@ -9,8 +9,6 @@ class HTTP:
 
 	def __init__(self, peticion):
 
-		
-
 		lista_peticion = peticion.splitlines()#Hacemos una lista con cada una de las lineas recibidas
 		metodo, url, version =lista_peticion[0].split()#Dividimos el primer string y guardamos cada campo en una variable
 		#En caso de no especificar el archivo se devuelve la página principal
@@ -25,7 +23,7 @@ class HTTP:
 		punto, ruta, extension = self.url.split('.')
 		#identificamos el tipo de extensión del archivo a enviar
 		if extension == 'css':
-			tipo_contenido = 'Content-type: text/css; charset=utf-8' 
+			tipo_contenido = 'Content-type: text/css; charset=UTF-8' 
 		elif extension == 'jpeg':
 			tipo_contenido = 'Content-type: image/jpeg'
 		elif extension == 'jpg':
@@ -39,7 +37,7 @@ class HTTP:
 		elif extension == 'mp3':
 			tipo_contenido = 'Content-type: audio/mp3'
 		else:
-		 	tipo_contenido = 'Content-type: text/html; charset=utf-8'
+		 	tipo_contenido = 'Content-type: text/html; charset=UTF-8'
 
 		try:
 			archivo = open(self.url, "r")
@@ -66,18 +64,19 @@ class HTTP:
 			mensaje = cabecera + contenido
 
 		elif(self.version == 'HTTP/1.1'):
-
+			rango = 'Accept-Ranges: bytes'
 			codigo = self.version + ' ' + '200 OK'
 			print codigo
 			format = "%A, %d-%b-%y %H:%M:%S"
 			fecha = 'Date: ' + datetime.datetime.today().strftime(format) + ' GMT'
 			servidor = 'Server: Python'
 			longitud = 'Content-lenght: ' + str(len(contenido))
-			conexion = 'Connection: keep-alive'
-			cabecera = [codigo,fecha, servidor,tipo_contenido, longitud, conexion,'\r\n']#creamos una lista con los campos de la cabecera
+			conexion = 'Connection: keep-alive\r\nKeep-Alive: timeout=5, max=1000'
+			cabecera = [codigo, rango, fecha, servidor,tipo_contenido, longitud, conexion,'\r\n']#creamos una lista con los campos de la cabecera
 			separador= '\r\n'
 			cabecera = separador.join(cabecera)#Juntamos los campos de la cabecera
 			mensaje = cabecera + contenido
+			print 'Inicio' + cabecera + 'Fin'
 
 		return mensaje
 
@@ -107,14 +106,16 @@ if __name__ == "__main__":
 		print 'en select'
 		read_sockets, write_sockets, error_sockets = select.select(socket_list , [], socket_list, timeout)
 		print 'saliendo select'
-
+		for sock in error_sockets:
+			print 'Error en socket'
 		for sock in read_sockets:
 			if sock == s:
 				#se recibe una peticion por el socket de escucha, lo aceptamos y lo añadimos a la lista de sockets
 				sockfd, addr = s.accept()
-				sockfd.setblocking(0)
+				#sockfd.setblocking(0)
 				socket_list.append(sockfd)
 				print 'nueva conexion'
+				
 			else:
 				peticion = sock.recv(4096)
 				print peticion
